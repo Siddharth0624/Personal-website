2538cf922cd2be9b11f27a30060d83b28162fe04
Siddharth Ray <sidharthray2001@gmail.com>
2021-07-26 20:34:50 +0530

    Delete js.js

--- js.js
+++
@@ -1,8 +0,0 @@
-burger = document.querySelector('.burger')
-navbar = document.querySelector('.navbar')
-navlist = document.querySelector('.navlist')
-burger.addEventListener('click', () => {
-    navlist.classList.toggle('vclass');
-    navbar.classList.toggle('hnav');
-
-})
